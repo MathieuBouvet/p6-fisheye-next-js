1ec983447d178235881c02e9d6ab271e1589e99b
Mathieu Bouvet <mathieubouvet42@gmail.com>
2022-07-15 19:56:27 +0200

    Redirect to homepage on logout

--- hooks/useLogout.ts
+++ hooks/useLogout.ts
@@ -1,15 +1,18 @@
 import { useSWRConfig } from "swr";
+import { useRouter } from "next/router";
 
 import apiRoutes from "@lib/routes/apiRoutes";
 import apiClient from "@lib/apiClient";
 
 function useLogout() {
   const { mutate } = useSWRConfig();
+  const router = useRouter();
 
   return async () => {
     await apiClient.post(apiRoutes.logout(), {});
     window.localStorage.removeItem("csrf_token");
-    mutate(apiRoutes.myProfile);
+    mutate(apiRoutes.myProfile());
+    router.push("/");
   };
 }
 
